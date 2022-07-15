3a602622cf50c9e267a2ace705f87e00bef3d380
krzysztoffila <umowy.fila@gmail.com>
2022-07-15 11:55:06 +0200

    Get 'href' attribute from the clicked link in js/script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
-// const { active } = require("browser-sync");
+'use strict';
 
 const titleClickHandler = function(event){
-    /*TARGET:  <ul class="list tags">
-                    <li><a href="#">design</a> <span>(6)</span></li>
-    currentTarget: null
-    */
+    event.preventDefault();
+
     const clickedElement = this;
     console.log(event);
     /* [DONE] remove class 'active' from all article links  */
@@ -12,19 +10,19 @@ const titleClickHandler = function(event){
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
-    /* [IN DROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
     clickedElement.classList.add('active');
-    
+
     console.log('clickedElement', clickedElement);
     console.log('clickedElement (with plus): ' + clickedElement);
     /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('article.active');
-
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
-    /* get 'href' attribute from the clicked link */
-
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
     /* find the correct article using the selector (value of 'href' attribute) */
 
     /* add class 'active' to the correct article */
